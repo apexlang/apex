c0c7d3cd9593e23e811641656c61706765f337cb
Jarrod Overson <jsoverson@gmail.com>
2022-12-16 13:28:46 -0500

    removed duplication, added generator test

--- test/plugin.test.ts
+++ test/plugin.test.ts
@@ -1,19 +1,25 @@
+import * as apex from "https://deno.land/x/apex_core@v0.1.1/mod.ts";
 import { assertEquals } from "https://deno.land/std@0.167.0/testing/asserts.ts";
-import { processPlugin } from "../src/plugins.ts";
+import { processPlugin, processConfig } from "../src/generate.ts";
 import * as path from "https://deno.land/std@0.167.0/path/mod.ts";
 
 const __dirname = new URL(".", import.meta.url).pathname;
+const spec = path.join(__dirname, "test.axdl");
+const plugin = path.join(__dirname, "test-plugin.ts");
+const generator = path.join(__dirname, "test-generator.ts");
 
 Deno.test(
   "plugin",
   { permissions: { read: true, net: true, run: true } },
   async () => {
-    const config = await processPlugin({
-      spec: path.join(__dirname, "test.axdl"),
-      plugins: [path.join(__dirname, "test-plugin.ts")],
+    const apexSource = await Deno.readTextFile(spec);
+    const doc = apex.parse(apexSource);
+    const config = await processPlugin(doc, {
+      spec,
+      plugins: [plugin],
     });
 
-    assertEquals(config.generates, { "dynamic.0.file": { module: "this.ts" } });
+    assertEquals(config.generates, { "Test.0.file": { module: generator } });
   }
 );
 
@@ -21,17 +27,43 @@ Deno.test(
   "plugin",
   { permissions: { read: true, net: true, run: true } },
   async () => {
-    const config = await processPlugin({
-      spec: path.join(__dirname, "test.axdl"),
-      plugins: [
-        path.join(__dirname, "test-plugin.ts"),
-        path.join(__dirname, "test-plugin.ts"),
-      ],
+    const apexSource = await Deno.readTextFile(spec);
+    const doc = apex.parse(apexSource);
+    const config = await processPlugin(doc, {
+      spec,
+      plugins: [plugin, plugin],
     });
 
     assertEquals(config.generates, {
-      "dynamic.0.file": { module: "this.ts" },
-      "dynamic.1.file": { module: "this.ts" },
+      "Test.0.file": { module: generator },
+      "Test.1.file": { module: generator },
     });
+  }
+);
+
+Deno.test(
+  "generate",
+  { permissions: { read: true, net: true, run: true } },
+  async () => {
+    const apexSource = await Deno.readTextFile(spec);
+    const output = await processConfig({
+      spec,
+      plugins: [plugin, plugin],
+    });
+
+    assertEquals(output, [
+      {
+        file: "Test.0.file",
+        source: "startend",
+        executable: false,
+        runAfter: undefined,
+      },
+      {
+        file: "Test.1.file",
+        source: "startend",
+        executable: false,
+        runAfter: undefined,
+      },
+    ]);
   }
 );
