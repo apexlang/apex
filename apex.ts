bf047e8fb4569de4cebaba3f827eedb0bd86cc1e
Jarrod Overson <jsoverson@gmail.com>
2023-01-23 10:25:40 -0500

    v0.0.15

--- apex.ts
+++ apex.ts
@@ -22,7 +22,7 @@ import { findApexConfig, setupLogger } from "./src/utils.ts";
 import { parseConfigYaml } from "./src/config.ts";
 
 // Version bump this on release.
-const version = "v0.0.14";
+const version = "v0.0.15";
 
 const args = Deno.args;
 
