5278e00fcac39d25d988a720f2cd4ef5e36927d7
pkedy <pkedy@users.noreply.github.com>
2025-03-21 13:24:03 +0000

    v0.2.6 release

--- README.md
+++ README.md
@@ -46,7 +46,7 @@ Output:
 
 ```console{title="apex help"}
 Usage:   apex                                                                                       
-Version: 0.2.5  
+Version: 0.2.6  
 
 Description:
 
--- mod.ts
+++ mod.ts
@@ -27,7 +27,7 @@ import { setupLogger } from "./src/utils.ts";
 import { RunError } from "./src/task.ts";
 
 // Version bump this on release.
-const version = "0.2.5";
+const version = "0.2.6";
 
 // This is necessary so we can modify the argument list.
 const args = Array.from(Deno.args);
